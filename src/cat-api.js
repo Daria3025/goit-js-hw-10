eee30eeb95818b8b6ef5ea267cac175cb7618f3e
Daria3025 <dtimofeeva191@icloud.com>
2023-08-08 22:55:01 +0300

    hw10

---
+++ src/cat-api.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_KEY =
+  'live_8fQ58DT49HCRjbfFAJl46zjJPL18fZrgCJNNV44Y8elBBTy3kk5pWvJJpWnCoc4D';
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+axios.defaults.headers.common['x-api-key'] = API_KEY;
+
+export function fetchBreeds() {
+  return axios.get(`${BASE_URL}/breeds`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.data;
+  });
+}
+
+export function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.data;
+    });
+}
